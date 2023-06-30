4181a94e823f64c709c922104372fbadeb20dfaa
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-06-30 12:08:05 +0800

    style(layout): configure layout page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import Nav from "@components/nav/Nav";
 import Footer from "@components/footer/Footer";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Web App",
@@ -10,10 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Nav />
-        {children}
-        <Footer />
+      <body className={inter.className}>
+        <div className="max-w-7xl min-h-screen my-0 mx-auto py-0 px-16 flex flex-col justify-between">
+          <Nav />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
