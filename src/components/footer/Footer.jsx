c143e4c8e6cc72a7c051a1802d5013a25ff7c1d4
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-06-30 10:37:08 +0800

    style(nav&footer): add skeleton for nav and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <p>©2023 Caesar Klidge Galutera. All rights reserved</p>
+    </div>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Link from "next/link";
 
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+function mapLinks(links) {
+  return links.map((link) => (
+    <Link key={link.id} href={link.url}>
+      {link.title}
+    </Link>
+  ));
+}
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div className="flex">
+      <Link href="/" className="mr-auto">Rovr</Link>
+      <div>{mapLinks(links)}</div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
