e0bd8ec7c832a652ef0f140f42b914bcca2778a7
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-03 10:14:36 +0800

    style(portfolio): add href to links

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -5,13 +5,13 @@ const Portfolio = () => {
     <div>
       <h1 className="px-0 py-5 text-lg font-bold">Choose a gallery</h1>
       <div className="flex gap-12 border-solid border-gray-300  ">
-        <Link href="" className="portfolio_link bg-link_image1 bg-cover ">
+        <Link href="/portfolio/illustrations" className="portfolio_link bg-link_image1 bg-cover ">
           <span className="portfolio-link__title">Illustrations</span>
         </Link>
-        <Link href="" className="portfolio_link bg-link_image2">
+        <Link href="portfolio/websites" className="portfolio_link bg-link_image2">
           <span className="portfolio-link__title">Websites</span>
         </Link>
-        <Link href="" className="portfolio_link bg-link_image3">
+        <Link href="portfolio/application" className="portfolio_link bg-link_image3">
           <span className="portfolio-link__title">Applications</span>
         </Link>
       </div>
