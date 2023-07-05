594769d9e3fbe09555e20073881d8c2c597cdcf2
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-05 09:26:02 +0800

    style(blog): add style to blog
    add style to dynamic pages of the blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Image from "next/image";
 
 const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <div>
+      <div className="flex">
+        <div className="flex-1 flex flex-col justify-between">
+          <h1 className="text-4xl">
+            Lorem ipsum dolor 
+          </h1>
+          <p className="text-lg font-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt dignissimos, laboriosam, minima maxime optio fugit consequatur porro assumenda natus nemo nulla! Excepturi quod harum voluptates quae modi vero eligendi dolorum!</p>
+          <div className="flex items-center gap-2">
+            <Image
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt="data_img"
+              width={40}
+              height={40}
+              className="object-cover rounded-full"
+            />
+            <span>John Doe</span>
+          </div>
+        </div>
+        <div className="flex-1 h-[18.75rem] relative">
+          <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" alt="" fill={true} className="object-cover" />
+        </div>
+      </div>
+      <div className="mt-12 text-lg font-light text-gray-500 text-justify ">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse adipisci fugit earum fuga error voluptas voluptatum cumque obcaecati cum, quia ea asperiores provident itaque nihil dolores dolorem quam iusto veniam.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div>
+      <Link href="/blog/testId" className="flex items-center gap-12 mb-12" >
+      <div className="">
+        <Image
+          src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+          alt=""
+          width={400}
+          height={250}
+          className="object-cover"
+        />
+      </div>
+      <div>
+        <h1 className="mb-2 text-5xl font-bold">Test</h1>
+        <p className="text-lg">Desc</p>
+      </div>
+      </Link>
+      <Link href="/blog/testId" className="flex items-center gap-12 mb-12" >
+      <div className="">
+        <Image
+          src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+          alt=""
+          width={400}
+          height={250}
+          className="object-cover"
+        />
+      </div>
+      <div>
+        <h1 className="mb-2 text-5xl font-bold">Test</h1>
+        <p className="text-lg">Desc</p>
+      </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
