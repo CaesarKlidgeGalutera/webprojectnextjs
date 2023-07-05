1034a14f80b154f63ac9333d9c2c01fd6fe211ca
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-05 08:53:16 +0800

    style(portfolio): add style to portfolio
    add style to dynamic pages of porfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-const Category = ({params}) => {
-  console.log(params)
+const Category = ({ params }) => {
   return (
-    <div>Category</div>
-  )
-}
+    <div className="">
+      <h1 className="text-primary-green text-3xl font-bold">
+        {params.category}
+      </h1>
+      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
+        <div className="flex-1 flex flex-col gap-5 justify-center">
+          <h1 className="text-3xl font-bold">Test</h1>
+          <p>Desc</p>
+          <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
+            See more
+          </button>
+        </div>
+        <div className="flex-1 h-[31.25rem] relative">
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt="ref_img"
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
+        <div className="flex-1 flex flex-col gap-5 justify-center">
+          <h1 className=" font-bold text-5xl">Test</h1>
+          <p className="text-lg">Desc</p>
+          <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
+            See more
+          </button>
+        </div>
+        <div className="flex-1 h-[31.25rem] relative">
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt="ref_img"
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
+        <div className="flex-1 flex flex-col gap-5 justify-center">
+          <h1 className="text-3xl font-bold">Test</h1>
+          <p>Desc</p>
+          <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
+            See more
+          </button>
+        </div>
+        <div className="flex-1 h-[31.25rem] relative">
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt="ref_img"
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
