c139af2945cf6273253ca736d07cb6dc2bc80dd0
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-16 09:40:14 +0800

    feat(api): created data api for portfolio categories

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+const getData = (category) => {
+  const data = items[category];
+
+  if(data){
+    return data;
+  }
+
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
   return (
     <div className="">
       <h1 className="text-primary-green text-3xl font-bold">
         {params.category}
       </h1>
-      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
+      {data.map((item) => (
+      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse" key={item.id}>
         <div className="flex-1 flex flex-col gap-5 justify-center">
-          <h1 className="text-3xl font-bold">Test</h1>
-          <p>Desc</p>
+          <h1 className="text-3xl font-bold">{item.title}</h1>
+          <p>={item.desc}</p>
           <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
             See more
           </button>
@@ -18,46 +32,13 @@ const Category = ({ params }) => {
         <div className="flex-1 h-[31.25rem] relative">
           <Image
             fill={true}
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            src={item.image}
             alt="ref_img"
             className="object-cover"
           />
         </div>
       </div>
-      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
-        <div className="flex-1 flex flex-col gap-5 justify-center">
-          <h1 className=" font-bold text-5xl">Test</h1>
-          <p className="text-lg">Desc</p>
-          <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
-            See more
-          </button>
-        </div>
-        <div className="flex-1 h-[31.25rem] relative">
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            alt="ref_img"
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <div className="flex gap-12 mb-24 mt-12 odd:flex-row-reverse">
-        <div className="flex-1 flex flex-col gap-5 justify-center">
-          <h1 className="text-3xl font-bold">Test</h1>
-          <p>Desc</p>
-          <button className="p-3 cursor-pointer bg-primary-green border-none rounded text-white w-max">
-            See more
-          </button>
-        </div>
-        <div className="flex-1 h-[31.25rem] relative">
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            alt="ref_img"
-            className="object-cover"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
