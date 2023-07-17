c3f21ebb63b10c5a0cb2c4e7650a8bef59d91f54
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-17 18:03:02 +0800

    refactor(db): refactor the name for postSchema

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "@utils/db";
-import Post from "@models/Post";
+import Post from "@models/postSchema";
 
 export const GET = async (req, res) => {
   try {
