c4866f1aaabfd487838c440d2599bc8599c0c89e
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-26 14:03:52 +0800

    feat(register):

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,18 +1,29 @@
 import connectDB from "@utils/db";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@models/user";
 
 export const POST = async (req) => {
   const { name, email, password } = await req.json();
 
   await connectDB();
 
+  const hashedPassword = await bcrypt.hash(password, 12);
+
   const newUser = new User({
-    
-  })
+    name,
+    email,
+    password: hashedPassword,
+  });
 
   try {
+    await newUser.save();
+
+    return new NextResponse("User created successfully", {
+      status: 201,
+    });
   } catch (error) {
-    return new NextResponse(err.message, {
+    return new NextResponse(error.message, {
       status: 500,
     });
   }
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [error, setError] = useState(false);
 
-export default Register
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      res.status === 201 &&
+        router.push("/dashboard/login?success=Account has been created");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <form
+        action=""
+        className="w-[18.75rem] flex flex-col gap-5"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="username"
+          required
+          className="p-5 border-2 border-solid rounded text-lg font-bold"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          required
+          className="p-5 border-2 border-solid rounded text-lg font-bold"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          className="p-5 border-2 border-solid rounded text-lg font-bold"
+        />
+        <button className="primary_btn p-5 text-lg font-bold" type="submit">
+          Register
+        </button>
+      </form>
+      {error && <p className="text-red-500">Something went wrong</p>}
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
+  );
+};
+
+export default Register;
