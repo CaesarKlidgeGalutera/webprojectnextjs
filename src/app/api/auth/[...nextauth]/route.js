2ed8cd4cd2bd78411996df1f8d2f07edcc3762c3
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-07-28 15:46:27 +0800

    feat(auth): add user authentication using credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from "@utils/db";
+import User from "@models/user";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +11,36 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connectDB();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isMatch = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Password not match");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -2,10 +2,43 @@
 import { signIn } from "next-auth/react";
 import React from "react";
 
+
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    signIn("credentials", { email, password });
+  };
   return (
-    <div>
-      <button onClick={() => signIn("google")}>Login with Google</button>
+    <div className="flex flex-col gap-5 items-center justify-center">
+      <form
+        action=""
+        className="w-[18.75rem] flex flex-col gap-5"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="email"
+          placeholder="email"
+          required
+          className="p-5 border-2 border-solid rounded text-lg font-bold"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          className="p-5 border-2 border-solid rounded text-lg font-bold"
+        />
+        <button className="primary_btn p-5 text-lg font-bold" type="submit">
+          Login
+        </button>
+      </form>
+      <button
+        onClick={() => signIn("google")}
+        className="border-solid border-black border-2 rounded"
+      >
+        Login with Google
+      </button>
     </div>
   );
 };
